21ea1955284c23e4d072060bb9b7a1873e8565b9
yasuyasu-ot <y.otsubo.dss@gmail.com>
2022-09-12 11:00:17 +0900

    #1  • mlapp 内に urls.py を作成  プロジェクトのulrs.pyを修正しmlappの方を見るように修正

--- mlproject/mlapp/urls.py
+++ mlproject/mlapp/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from django.http import HttpResponse
+
+def test(request):
+  return HttpResponse('テストページ')
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-        #mlappを参照
-    path('',include('mlapp.urls'))
+    # テスト用
+    path('test/', test)
 ]
