b247b29b0774949845c9b2761a71fe763de9087f
yasuyasu-ot <y.otsubo.dss@gmail.com>
2022-09-18 00:36:28 +0900

    審査結果画面実装 #9 result画面への遷移を追加

--- mlproject/mlapp/views.py
+++ mlproject/mlapp/views.py
@@ -6,11 +6,14 @@ def index(request):
 
 def input_form(request):
     # 下記の様に編集
-    if request.method == "POST": # Formの入力があった時、
-        form = InputForm(request.POST) # 入力データを取得する。
-        if form.is_valid(): # Formの記載の検証を行い、
-            form.save() # 問題なければ、入力データを保存
-            return redirect('index') # 保存後遷移するページの指定
+    if request.method == 'POST':
+        form = InputForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('result') # /resultへ遷移するように変更
     else:
         form = InputForm()
         return render(request, 'mlapp/input_form.html', {'form':form})
+
+def result(request):
+    return render(request, 'mlapp/result.html')
